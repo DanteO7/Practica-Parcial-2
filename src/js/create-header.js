59c976fc988a7bbf1ca2ee36a5b9db53f2d7eac3
Dante07 <dante.orsetti@gmail.com>
2025-07-01 11:16:34 -0300

    Práctica Parcial 2

--- src/js/create-header.js
+++ src/js/create-header.js
@@ -1,13 +1,13 @@
 export function createHeader(header) {
   header.innerHTML = `
     <div class="logo-container">
-          <a href="./../../index.html"><img src="/assets/liev.png" alt="Logo de Liev" /></a>
+          <a href="/index.html"><img src="/assets/liev.png" alt="Logo de Liev" /></a>
         </div>
         <div class="input-container">
           <input type="search" id="search-input" />
         </div>
         <div class="favorites-container">
-          <a href="./../../favorites.html"><img src="/assets/favoritos.png" alt="Imagen de favoritos" /></a>
+          <a href="/favorites.html"><img src="/assets/favoritos.png" alt="Imagen de favoritos" /></a>
         </div>
   `;
 }
