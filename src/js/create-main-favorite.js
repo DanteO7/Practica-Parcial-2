1c9e5918bab2f6292f18d1c8cb13aad05ad4a54a
Dante07 <dante.orsetti@gmail.com>
2025-07-03 18:29:46 -0300

    Práctica Parcial 2

--- src/js/create-main-favorite.js
+++ src/js/create-main-favorite.js
@@ -13,18 +13,20 @@ export function createMainFavorite(main) {
   cardContainer.classList.add("cards-container");
 
   // recorre el local storage para buscar los juegos que el usuario marcó como favoritos y los guarda el value en un array
-  function getLocalStorage() {
-    const data = [];
-    for (let i = 0; i < localStorage.length; i++) {
-      const key = localStorage.key(i);
-      const value = localStorage.getItem(key);
-      data.push(JSON.parse(value));
-    }
-    return data;
+  const data = [];
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    const value = localStorage.getItem(key);
+    data.push(JSON.parse(value));
   }
 
+  if (data.length === 0) {
+    console.log(data);
+    cardContainer.innerHTML = `<h3>No tienes juegos favoritos</h3>`;
+  } else {
+    createCard(data, cardContainer);
+  }
   // crea las cards según los juegos que encontró en el local storage
-  createCard(getLocalStorage(), cardContainer);
 
   // agrega los elementos al main
   main.appendChild(titleContainer);
